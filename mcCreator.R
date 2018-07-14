1d7f68879228bfede12cca9c615e05a24d971710
Christian Adriano, M.Sc., PMP <christian.adriano@gmail.com>
2018-07-14 16:58:08 +0200

    Testing the Markovchain library

--- mcCreator.R
+++ mcCreator.R
@@ -4,7 +4,7 @@
 install.packages("markovchain")
 library(markovchain)
 
-tmA <- matrix(c(0.4,0.3,0.3,
+tmA <- matrix(c(0.1,0.6,0.3,
                 0.3,0.4,0.3,
                 0.3,0.3,0.4),
               nrow = 3,
@@ -12,8 +12,18 @@ tmA <- matrix(c(0.4,0.3,0.3,
 
 dtmcA <- new("markovchain",transitionMatrix=tmA,
              states=c("yes","no","idk"),
-             name="MarkovChain A") #create the DTMC
+             name="MarkovChain A") #create  the DTMC
 dtmcA
-plot(dtmcA, col="red",pch=16)
+p <- plot(dtmcA, col="red",pch=16)
+p
 
 dtmcA["yes","no"] #Probability of transitionting from Yes to No.
+
+#Simulate states distribution after n-steps
+initialState<-c(0,1,0)
+steps<-2
+finalState<-initialState*dtmcA^steps #using power operator
+finalState
+
+# Steady states 
+steadyStates(dtmcA) #
